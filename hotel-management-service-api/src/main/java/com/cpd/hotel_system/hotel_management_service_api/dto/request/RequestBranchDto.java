56713a2635f0131429a61cd59422d4a8f9527803
Vinali Samarasinghe <it21321368@my.sliit.lk>
2025-08-27 14:24:49 +0530

    added request package files

---
+++ hotel-management-service-api/src/main/java/com/cpd/hotel_system/hotel_management_service_api/dto/request/RequestBranchDto.java
@@ -0,0 +1,19 @@
+package com.cpd.hotel_system.hotel_management_service_api.dto.request;
+
+import com.cpd.hotel_system.hotel_management_service_api.enums.BranchType;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RequestBranchDto {
+    private String branchName;
+    private BranchType branchType;
+    private int RoomCount;
+    private String hotelId;
+
+}
